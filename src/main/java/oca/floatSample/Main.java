b0c68b1f487947ddd71006e3f8d9f27f0727b33b
c-alexmala <c-alexandru.malai@edifecs.com>
2018-01-18 19:01:28 +0200

    oca: float samples

---
+++ src/main/java/oca/floatSample/Main.java
@@ -0,0 +1,12 @@
+package oca.floatSample;
+
+public class Main {
+    public static void main(String[] args) {
+        float value1 = 102;
+        float value2 = (int)102;
+//        float value3 = 1F * 0.0;
+        float value4 = 1F * (short)0.0;
+//        float ii = 1.0;
+//        float value5 = 1F * (boolean)0;
+    }
+}
